ccbdb925ccad3b42401d3f81a334d6f02a83c634
Tony Grue <tonygrue@hotmail.com>
2019-06-03 22:07:36 -0700

    Initial Checkin

---
+++ src/main.rs
@@ -0,0 +1,62 @@
+const NUM_LV1: u128 = 25;
+const NUM_LV2: u128 = 25;
+const NUM_LV3: u128 = 25;
+const NUM_DOMES: u128 = 21;
+
+const BOARD_DIM: u128 = 5;
+
+fn nCr(n: u128, r: u128) -> u128 {
+    assert!(n >= r);
+    if r == 0 {return 1};
+    return (n * nCr(n-1, r-1)) / r;
+}
+
+// fn main() {
+//     println!("nCr 1C1: {}", nCr(1, 1));
+//     println!("nCr 2C1: {}", nCr(2, 1));
+//     println!("nCr 2C2: {}", nCr(2, 2));
+//     println!("nCr 3C1: {}", nCr(3, 1));
+//     println!("nCr 3C2: {}", nCr(3, 2));
+//     println!("nCr 3C3: {}", nCr(3, 3));
+//     println!("nCr 5C1: {}", nCr(5, 1));
+//     println!("nCr 5C2: {}", nCr(5, 2));
+//     println!("nCr 5C3: {}", nCr(5, 3));
+//     println!("nCr 5C4: {}", nCr(5, 4));
+//     println!("nCr 5C5: {}", nCr(5, 5));
+// }
+// // nCr 1C1: 1
+// // nCr 2C1: 2
+// // nCr 2C2: 1
+// // nCr 3C1: 3
+// // nCr 3C2: 3
+// // nCr 3C3: 1
+// // nCr 5C1: 5
+// // nCr 5C2: 10
+// // nCr 5C3: 10
+// // nCr 5C4: 5
+// // nCr 5C5: 1
+
+
+
+fn count_recursive_combos(n: u128, r: u128, levels: u128) -> u128{
+    if levels == 0 {return 1};
+
+    let mut combos: u128 = 0;
+    for r in 1..=n {
+        combos += nCr(n, r) * count_recursive_combos(r, r, levels - 1);
+    }
+    return combos;
+}
+
+fn count_base_combos() -> u128 {
+    let mut base_combos: u128 = 0;
+    for r in 1..=NUM_LV1 {
+        base_combos += nCr(BOARD_DIM * BOARD_DIM, r);
+    }
+    return base_combos;
+}
+
+fn main() {
+    println!("Base Combinations: {}", count_base_combos());
+    println!("Recursive Combinations: {}", count_recursive_combos(25, 25, 4));
+}
